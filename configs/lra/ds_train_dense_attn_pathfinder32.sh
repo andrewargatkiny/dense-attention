dbfddd28fbb8691f4dbc9d00d670ccbcff59c579
Mirantyl <alekseevp03@gmail.com>
2025-06-27 00:07:57 +0300

    Add logic to handle parameters

--- configs/lra/ds_train_dense_attn_pathfinder32.sh
+++ configs/lra/ds_train_dense_attn_pathfinder32.sh
@@ -28,7 +28,7 @@ if [ "${1-}" = "--resume" ]; then
     LOAD_EPOCH=""
   fi
 
-  [ $# -ge 1 ] || { echo "Usage: $0 --resume [EPOCH|last] JOB_NAME"; exit 1; }
+  [ $# -ge 1 ] && [ "$1" != "--override" ] || { echo "Usage: $0 --resume [EPOCH|last] JOB_NAME"; exit 1; }
   SUBDIR=$1; shift
 
   if [ "${1-}" = "--override" ]; then
