e2d2723ed8904685a2f40ed4488446a5e88094c1
Andrew Argatkiny <andrewkrskde@gmail.com>
2025-06-26 03:51:40 +0200

    fix small mistakes with new configs

--- configs/gpt/ds_train_transformer_gpt_360m_bf16.sh
+++ configs/gpt/ds_train_transformer_gpt_360m_bf16.sh
@@ -8,7 +8,7 @@ BASE_JOB_NAME="gpt_pretraining"
 SEED=${SEED:-42}
 #NODE=${NODE:-0}
 MASTER_PORT=${MASTER_PORT:-29500}
-CONFIG=${CONFIG:-${base_dir}/configs/gpt/llama340m.json}
+CONFIG=${CONFIG:-${base_dir}/configs/gpt/llama_340m.json}
 DS_CONFIG=${DS_CONFIG:-${base_dir}/configs/gpt/deepspeed_transformer_4k.json}
 
 MODEL_CONFIG=${MODEL_CONFIG:-"$CONFIG"}
