50c818370023242ee2b6b46608a9325106e82a47
Andrew Argatkiny <andrewkrskde@gmail.com>
2025-10-06 18:57:04 +0200

    Fix the bug with _forward_chunked when seq_len is not divisible by chunk_size

--- src/dense_attention.py
+++ src/dense_attention.py
@@ -442,7 +442,7 @@ class DenseAttention(nn.Module):
         last_chunk =  seq_len - chunk_size * num_chunks
         if last_chunk != 0:
             remainder_dims = list(size)
-            remainder_dims[1] = last_chunk
+            remainder_dims[1] = chunk_size - last_chunk
             remainder = torch.zeros(
                 size=remainder_dims, device=hidden_states.device,
                 dtype=hidden_states.dtype
@@ -491,7 +491,7 @@ class DenseAttention(nn.Module):
         output = self.permute_dims_f(attention)
         # output: (Batch, Chunk, ChunkLen, EmbedDim) or
         # (Batch, Chunk, ChunkLen, Head, HeadDim)
-        output = output.reshape(size[0], seq_len + last_chunk, size[2])
+        output = output.reshape(size[0], -1, size[2])
         output = output[:, :seq_len, :]
         # output: Batch, SeqLen, EmbedDim
         return output
