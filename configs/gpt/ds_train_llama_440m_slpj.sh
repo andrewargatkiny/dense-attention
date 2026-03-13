7ca77505becfb668ea4e6dc2af141c426624e4fa
Andrew Argatkiny <andrewkrskde@gmail.com>
2026-03-13 20:19:40 +0100

    fix transformer config

--- configs/gpt/ds_train_llama_440m_slpj.sh
+++ configs/gpt/ds_train_llama_440m_slpj.sh
@@ -9,7 +9,7 @@ SEED=${SEED:-42}
 #NODE=${NODE:-0}
 MASTER_PORT=${MASTER_PORT:-29500}
 CONFIG=${CONFIG:-${base_dir}/configs/gpt/llama_440m_slimpajama.json}
-DS_CONFIG=${DS_CONFIG:-${base_dir}/configs/gpt/deepspeed_transformer_2k.json}
+DS_CONFIG=${DS_CONFIG:-${base_dir}/configs/gpt/deepspeed_2k_440m.json}
 
 MODEL_CONFIG=${MODEL_CONFIG:-"$CONFIG"}
 DATA_CONFIG=${DATA_CONFIG:-"$CONFIG"}
